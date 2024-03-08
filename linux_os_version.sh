119927dc8e085012c17da486cb4313671cecb381
Alexandre Marques Menezes Malaquias <a.menezes.malaquias@avanade.com>
2024-03-08 11:08:42 -0300

    Corrigindo script linux_os_version.sh

--- linux_os_version.sh
+++ linux_os_version.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-os_version=$(cat /etc/os-release | grep -oP '(?<=VERSION_ID=")\d+\.\d+' | head -1)
+# Check if the OS is Linux
+if [[ "$(uname)" == "Linux" ]]; then
+    # Check if the OS is Ubuntu
+    if [[ -f "/etc/lsb-release" ]]; then
+        source /etc/lsb-release
+        os_version="Ubuntu $DISTRIB_RELEASE"
+    # Check if the OS is CentOS
+    elif [[ -f "/etc/centos-release" ]]; then
+        os_version=$(cat /etc/centos-release)
+    # Check if the OS is Fedora
+    elif [[ -f "/etc/fedora-release" ]]; then
+        os_version=$(cat /etc/fedora-release)
+    # Check if the OS is Red Hat
+    elif [[ -f "/etc/redhat-release" ]]; then
+        os_version=$(cat /etc/redhat-release)
+    # Check if the OS is Debian
+    elif [[ -f "/etc/debian_version" ]]; then
+        os_version=$(cat /etc/debian_version)
+    else
+        os_version="Unknown Linux distribution"
+    fi
+# Check if the OS is macOS
+elif [[ "$(uname)" == "Darwin" ]]; then
+    os_version=$(sw_vers -productVersion)
+# Check if the OS is Windows
+elif [[ "$(uname -o)" == "Msys" ]]; then
+    os_version="Windows"
+else
+    os_version="Unknown operating system"
+fi
 
-echo "Linux OS version: $os_version"
-
+echo "OS version: $os_version"
